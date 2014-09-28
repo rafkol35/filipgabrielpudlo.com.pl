de0d43d5b9c39a7f610bd10f098e3061d48a6428
rafkol <rafalkolasa@o2.pl>
2014-09-28 11:55:34 +0200

    dalsze prace

--- application/controllers/panel/project.php
+++ application/controllers/panel/project.php
@@ -13,51 +13,37 @@ class Project extends MY_Controller {
         $this->dataPage = array();
     }
 
-    function photos($whichID){
-        $this->load->model('MGalleries', 'MGalleries', TRUE);
-        $gallery = $this->MGalleries->get($whichID);
-
-        $data['innerJSs'] = array('panel/project/photos.php');
-        /*$data['jscripts'] = array('swfupload/swfupload.js','handlers.js');*/
-        $data['galleryID'] = $whichID;
-        $data['title'] = $gallery->title_pl.' : Zdjęcia';
-        $data['styles'] = array('default.css');
-
-        //$data2['photos'] = $this->MGalleries->get_photos($whichID);
-        //$this->load->model('MPhotos','MPhotos',TRUE);
-        //$data['photos'] = $this->MPhotos->get_photos($whichID);
-        
-        $data2['gallery'] = $gallery;
-        $data2['slideSpeed'] = $gallery->slideSpeed;
-        $data2['cts'] = $this->MGalleries->getAll();
-
-        $this->load->view('panel/header',$data);
-        $this->load->view('panel/project/photos',$data2);
-        $this->load->view('panel/footer');
-    }
+//    function photos($whichID){
+//        $this->dataPage['project'] = $this->MProjects->get($whichID);
+//        $this->dataPage['projectID'] = $whichID;
+//
+//        $this->dataHead['innerJSs'] = array('panel/project/photos.php');
+//        $this->dataHead['title'] = $this->dataPage['project']->title_pl.' : Zdjęcia';
+//        //$data['styles'] = array('default.css');
+//
+//        //$data2['photos'] = $this->MGalleries->get_photos($whichID);
+//        //$this->load->model('MPhotos','MPhotos',TRUE);
+//        //$data['photos'] = $this->MPhotos->get_photos($whichID);
+//        
+//        //$data2['gallery'] = $gallery;
+//        //$data2['slideSpeed'] = $gallery->slideSpeed;
+//        //$data2['cts'] = $this->MGalleries->getAll();
+//
+//        $this->load->view('panel/header',$this->dataHead);
+//        $this->load->view('panel/project/photos',$this->dataPage);
+//        $this->load->view('panel/footer');
+//    }
 
     function textEdit($whichID){
-        $project = $this->MProjects->get($whichID);
-
+        $this->dataPage['project'] = $this->MProjects->get($whichID);
+        $this->dataPage['projectID'] = $whichID;
+        
         $this->dataHead['innerJSs'] = array('panel/project/text.php');
         $this->dataHead['jscripts'] = array('tinymce/tinymce.min.js');
         $this->dataHead['galleryID'] = $whichID;
-        
-        $this->dataPage['project'] = $project;
-        $this->dataPage['projectID'] = $whichID;
     }
     
     function fullDesc($whichID){
-//        $project = $this->MProjects->get($whichID);
-//
-//        $data['innerJSs'] = array('panel/project/text.php');
-//        $data['jscripts'] = array('tiny_mce/tiny_mce.js');
-//        $data['galleryID'] = $whichID;
-//        $data['title'] = $project->title_pl.' : Tekst';
-//
-//        $data2['projectID'] = $whichID;
-//        $data['project'] = $project;
-
         $this->textEdit($whichID);
         $this->dataHead['title'] = $this->dataPage['project']->title_pl.' : Pełny opis projektu';
 
@@ -67,18 +53,6 @@ class Project extends MY_Controller {
     }
 
     function shortDesc($whichID){
-//        $gallery = $this->MGalleries->get($whichID);
-//
-//        $data['innerJSs'] = array('panel/project/text.php');
-//        $data['jscripts'] = array('tiny_mce/tiny_mce.js');
-//        $data['galleryID'] = $whichID;
-//        $data['title'] = $gallery->title_pl.' : Krótki tekst';
-//
-//        $data2['galleryID'] = $whichID;
-//        $data['gallery'] = $gallery;
-//
-//        $this->textEdit($whichID);
-        
         $this->textEdit($whichID);
         $this->dataHead['title'] = $this->dataPage['project']->title_pl.' : Skrótowy opis projektu';
         
